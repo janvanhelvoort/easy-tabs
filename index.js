a7f66b8cc2cef8aa8956eccabc7a014e80f402db
Jan van Helvoort <janvanhelvoort@outlook.com>
2016-04-21 09:22:51 +0200

    remove index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Tabs: require('./src/components/tabs.jsx'),
-    TabList: require('./src/components/tabList.jsx'),
-    Tab: require('./src/components/tab.jsx'),
-    PanelContainer: require('./src/components/panelContainer.jsx'),
-    Panel: require('./src/components/panel.jsx')
-}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Tabs: require('./components/tabs'),
+    TabList: require('./components/tabList'),
+    Tab: require('./components/tab'),
+    PanelContainer: require('./components/panelContainer'),
+    Panel: require('./components/panel')
+}
